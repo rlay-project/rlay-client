cc92c538842ad762fc21016df442c404e463395c
Maximilian Goisser <goisser94@gmail.com>
2018-10-16 09:44:09 +0200

    feat(doctor): check addresses for ABI compatibility instead bytecode

--- src/doctor.rs
+++ src/doctor.rs
@@ -1,3 +1,4 @@
+use ethabi;
 use console::{style, Emoji};
 use failure::{err_msg, Error};
 use rustc_hex::FromHex;
@@ -29,6 +30,62 @@ pub fn check_address_code(
     let bytecode_equal = ontology_code.0 == content_bytes;
 
     Ok(bytecode_equal)
+}
+
+fn input_param_types(function: &ethabi::Function) -> Vec<ethabi::ParamType> {
+    function.inputs.iter().map(|p| p.kind.clone()).collect()
+}
+
+fn short_signature(name: &str, params: &[ethabi::ParamType]) -> [u8; 4] {
+    let mut result = [0u8; 4];
+    fill_signature(name, params, &mut result);
+    result
+}
+
+fn fill_signature(name: &str, params: &[ethabi::ParamType], result: &mut [u8]) {
+    let types = params
+        .iter()
+        .map(ethabi::param_type::Writer::write)
+        .collect::<Vec<String>>()
+        .join(",");
+
+    let data: Vec<u8> = From::from(format!("{}({})", name, types).as_str());
+
+    let mut sponge = ::tiny_keccak::Keccak::new_keccak256();
+    sponge.update(&data);
+    sponge.finalize(result);
+}
+
+fn function_signature(function: &ethabi::Function) -> ethabi::Result<[u8; 4]> {
+    let params = input_param_types(function);
+
+    Ok(short_signature(&function.name, &params))
+}
+
+pub fn check_address_abi(
+    eloop: &mut tokio_core::reactor::Core,
+    web3: &web3::Web3<impl Transport>,
+    address: H160,
+    abi: &str,
+) -> Result<bool, Error> {
+    let address_code = eloop
+        .run(web3.eth().code(address, None))
+        .map_err(|_| err_msg("Failed to fetch contract code"))?;
+    // println!("ADDRESS CODE: {:?}", address_code.0.to_hex());
+
+    let contract = ethabi::Contract::load(abi.as_bytes()).unwrap();
+    for function in contract.functions() {
+        let signature = function_signature(function).unwrap();
+        let position = address_code
+            .0
+            .windows(signature.to_vec().len())
+            .position(|window| window == signature.to_vec().as_slice());
+        if position.is_none() {
+            return Ok(false);
+        }
+    }
+
+    Ok(true)
 }
 
 pub fn print_contract_check(
@@ -67,27 +124,27 @@ pub fn check_contracts(
         return;
     }
 
-    let mut contract_bytecodes = HashMap::new();
-    contract_bytecodes.insert(
+    let mut contract_abis = HashMap::new();
+    contract_abis.insert(
         "OntologyStorage",
-        include_str!("../data/OntologyStorage.bin"),
+        include_str!("../data/OntologyStorage.abi"),
     );
-    contract_bytecodes.insert("RlayToken", include_str!("../data/RlayToken.bin"));
-    contract_bytecodes.insert(
+    contract_abis.insert("RlayToken", include_str!("../data/RlayToken.abi"));
+    contract_abis.insert(
         "PropositionLedger",
-        include_str!("../data/PropositionLedger.bin"),
+        include_str!("../data/PropositionLedger.abi"),
     );
 
-    let mut contract_deployed: HashMap<&str, Result<bool, Error>> = HashMap::new();
-    for (name, bytecode) in contract_bytecodes {
+    let mut contract_matches_abi: HashMap<&str, Result<bool, Error>> = HashMap::new();
+    for (name, abi) in contract_abis {
         let address_hash = config.contract_address(name);
-        let is_deployed = check_address_code(eloop, &web3, address_hash, bytecode);
-        contract_deployed.insert(name, is_deployed);
+        let matches_abi = check_address_abi(eloop, &web3, address_hash, abi);
+        contract_matches_abi.insert(name, matches_abi);
     }
 
-    println!("Checking contracts:");
-    for (name, is_deployed) in contract_deployed {
-        print_contract_check(name, &config.contract_addresses[name], &is_deployed);
+    println!("Checking contract ABIs:");
+    for (name, matches_abi) in contract_matches_abi {
+        print_contract_check(name, &config.contract_addresses[name], &matches_abi);
     }
 }
 
--- update_data.sh
+++ update_data.sh
@@ -4,10 +4,7 @@ set -euxo pipefail
 source_dir=$1
 
 cat $source_dir/OntologyStorage.json | jq .abi > data/OntologyStorage.abi
-cat $source_dir/OntologyStorage.json | jq .deployedBytecode -r > data/OntologyStorage.bin
 
 cat $source_dir/RlayToken.json | jq .abi > data/RlayToken.abi
-cat $source_dir/RlayToken.json | jq .deployedBytecode -r > data/RlayToken.bin
 
 cat $source_dir/PropositionLedger.json | jq .abi > data/PropositionLedger.abi
-cat $source_dir/PropositionLedger.json | jq .deployedBytecode -r > data/PropositionLedger.bin
