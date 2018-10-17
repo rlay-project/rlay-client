76f94044d605be3f847996854823475f87dfe73c
Maximilian Goisser <goisser94@gmail.com>
2018-10-17 09:33:33 +0200

    feat(init): add init subcommand

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate byteorder;
 extern crate cid;
 extern crate clap;
 extern crate console;
+extern crate dialoguer;
 extern crate env_logger;
 extern crate ethabi;
 extern crate failure;
@@ -36,6 +37,7 @@ extern crate web3;
 pub mod aggregation;
 pub mod config;
 pub mod doctor;
+pub mod init;
 pub mod merkle;
 pub mod ontology_ext;
 pub mod payout;
@@ -101,6 +103,9 @@ fn main() {
                 .about("Diagnose problems by running a series of checks")
                 .arg(&config_path_arg),
         )
+        .subcommand(
+            SubCommand::with_name("init").about("Initialize a directory as a project using Rlay"),
+        )
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("client") {
@@ -112,6 +117,8 @@ fn main() {
         let config_path = matches.value_of("config_path");
         let config = config::Config::from_path_opt(config_path).expect("Couldn't read config file");
         doctor::run_checks(&config);
+    } else if let Some(_) = matches.subcommand_matches("init") {
+        init::init();
     } else if let Some(matches) = matches.subcommand_matches("payout") {
         let config_path = matches.value_of("config_path");
         let config = config::Config::from_path_opt(config_path).expect("Couldn't read config file");
